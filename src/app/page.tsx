5cf6f84928a06acf2c44a118dc217c4400b68169
kirill-voronin-study <kirill223v46@gmail.com>
2026-08-04 18:23:11 +0300

    feat: add card layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
-import { Card } from "@/components";
 import styles from "./page.module.css";
 import LikeWrapper from "./components/like-wrapper";
+import CardsLayout from "./components/cards-layout/cards-layout";
 
 export default function Home() {
   return (
     <main className={styles.container}>
-      <Card
-        img="/grid.png"
-        topic="Front-end"
-        date="1 месяц назад"
-        like={4}
-        title="Как работать с CCS Grid"
-        content="Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы.."
-        timeToRead="3 минуты"
-      />
-      <LikeWrapper />
+      <CardsLayout />
+      <div className={styles["like-container"]}>
+        <LikeWrapper />
+      </div>
     </main>
   );
 }
